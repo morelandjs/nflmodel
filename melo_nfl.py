350f1cd98b8c7986e4d55a682cd67c2d25becc27
J. Scott Moreland <morelandjs@gmail.com>
2019-07-14 16:40:12 -0400

    Update for published melo package

--- melo_nfl.py
+++ melo_nfl.py
@@ -1,100 +1,112 @@
 #!/usr/bin/env python2.7
 
-from datetime import datetime, timedelta
-from pathlib import Path
-
+from melo import Melo
 import nfldb
 import numpy as np
-from skopt import gp_minimize
+import pandas as pd
+from pyDOE import lhs
 
-from melo import Melo
 
-
+# Load NFL game data
 db = nfldb.connect()
-q = nfldb.Query(db)
-q.game(season_type='Regular', finished=True)
+query = nfldb.Query(db)
+query.game(season_type='Regular', finished=True)
 
-dates = [g.start_time for g in q.as_games()]
-labels1 = [g.home_team for g in q.as_games()]
-labels2 = [g.away_team for g in q.as_games()]
-spreads = [g.home_score - g.away_score for g in q.as_games()]
-totals = [g.home_score + g.away_score for g in q.as_games()]
+fields = ['start_time', 'home_team', 'home_score', 'away_team', 'away_score']
+
+games = pd.DataFrame([
+    tuple(getattr(g, f) for f in fields)
+    for g in sorted(query.as_games(), key=lambda g: g.start_time)
+], columns=fields)
+
+# Add columns to track rest days
+teams = np.union1d(games.home_team, games.away_team)
+time = {team: games.start_time.min() for team in teams}
+
+for index, game in games.iterrows():
+    home_rest, away_rest = [
+        (game.start_time - time[getattr(game, team)]).days
+        for team in ('home_team', 'away_team')
+    ]
+    for team in ('home_team', 'away_team'):
+        time[getattr(game, team)] = game.start_time
+
+    games.at[index, 'home_rest'] = home_rest if index > 16 else 7
+    games.at[index, 'away_rest'] = away_rest if index > 16 else 7
 
 
-def melo_wrapper(mode, k, bias, decay, smooth, verbose=False):
+def melo_wrapper(mode, kfactor, home_bias, decay_rate, fatigue):
     """
-    Thin wrapper to pass arguments to the Melo library.
+    Melo class wrapper.
 
     """
-    values, lines = {
-        'fermi': (spreads, np.arange(-59.5, 60.5)),
-        'bose': (totals, np.arange(-0.5, 105.5)),
-    }[mode]
+    if mode == 'spreads':
+        # comparison lines
+        lines = np.arange(-59.5, 60.5)
 
-    return Melo(
-        dates, labels1, labels2, values, lines=lines,
-        mode=mode, k=k, bias=bias, smooth=smooth,
-        decay=lambda t: 1 if t < timedelta(weeks=20) else decay
+        # comparison bias factors
+        home_fatigue = fatigue * np.exp(-games.home_rest / 7.0)
+        away_fatigue = fatigue * np.exp(-games.away_rest / 7.0)
+        biases = home_bias - fatigue * (home_fatigue - away_fatigue)
+
+        # point spreads anti-commute
+        commutes = False
+
+        # pairwise comparison statistic
+        comparisons = games.home_score - games.away_score
+    elif mode == 'totals':
+        # comparison lines
+        lines = np.arange(-0.5, 101.5)
+
+        # comparison bias factors
+        home_fatigue = fatigue * np.exp(-games.home_rest / 7.0)
+        away_fatigue = fatigue * np.exp(-games.away_rest / 7.0)
+        biases = home_bias - fatigue * (home_fatigue + away_fatigue)
+
+        # point totals commute
+        commutes = True
+
+        # pairwise comparison statistic
+        comparisons = games.home_score + games.away_score
+    else:
+        raise ValueError('no such mode')
+
+    def regress(years):
+        return 1 - np.exp(-years/(decay_rate + 1e-12))
+
+    regress_unit = 'year'
+
+    model = Melo(kfactor, lines=lines, sigma=1.0, regress=regress,
+                 regress_unit=regress_unit, commutes=commutes)
+
+    model.fit(
+        games.start_time,
+        games.home_team,
+        games.away_team,
+        comparisons,
+        biases,
     )
 
+    return model
 
-def from_cache(mode, retrain=False, **kwargs):
-    """
-    Load the melo args from the cache if available, otherwise
-    train and cache a new instance.
 
-    """
-    cachefile = Path('cachedir', '{}.cache'.format(mode.lower()))
-
-    if not retrain and cachefile.exists():
-        args = np.loadtxt(cachefile)
-        return melo_wrapper(mode, *args)
-
-    def obj(args):
-        melo = melo_wrapper(mode, *args)
-        return melo.entropy()
-
-    x0 = {
-        'fermi': (0.20, 0.15, 0.60, 7.0),
-        'bose': (0.20, 0.00, 0.60, 7.0),
-    }[mode]
-
-    bounds = {
-        'fermi': [(0.1, 0.3), (0.1, 0.2), (0.55, 0.75), (0.0, 15.0)],
-        'bose': [(0.1, 0.3), (-0.01, 0.01), (0.55, 0.75), (0.0, 15.0)],
-    }[mode]
-
-    res = gp_minimize(obj, bounds, n_calls=100, n_jobs=4, verbose=True)
-
-    print("mode: {}".format(mode))
-    print("best mean absolute error: {:.4f}".format(res.fun))
-    print("best parameters: {}".format(res.x))
-
-    if not cachefile.parent.exists():
-        cachefile.parent.mkdir()
-
-    np.savetxt(cachefile, res.x)
-    return melo_wrapper(mode, *res.x)
+nfl_spreads = melo_wrapper('spreads', .166, .182, 3.48, 0.514)
+nfl_totals = melo_wrapper('totals', .130, 0.064, 0.775, 0.276)
 
 
 if __name__ == "__main__":
-    import argparse
 
-    parser = argparse.ArgumentParser(
-        description='calibrate model parameters for point spreads and totals',
-        argument_default=argparse.SUPPRESS
-    )
+    labels, limits = map(list, zip(*[
+        ('k',       (0.0,  0.5)),
+        ('bias',    (0.0,  0.5)),
+        ('decay',   (0.0,  1.0)),
+        ('fatigue', (0.0,  1.0)),
+    ]))
 
-    parser.add_argument(
-        '--retrain', action='store_true', default=False,
-        help='retrain even if model args are cached'
-    )
+    xmin, xmax = map(np.array, zip(*limits))
+    X = xmin + (xmax - xmin) * lhs(len(labels), samples=1000)
+    y = np.array([melo_wrapper('totals', *x).loss for x in X])
 
-    args = parser.parse_args()
-    kwargs = vars(args)
-
-    for mode in 'fermi', 'bose':
-        from_cache(mode, **kwargs)
-else:
-    nfl_spreads = from_cache('fermi')
-    nfl_totals = from_cache('bose')
+    xopt = X[y.argsort()][:20].mean(axis=0)
+    for label, x in zip(labels, xopt):
+        print('{}: {}'.format(label, x))
--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
-setup(name='Melo NFL',
+setup(name='MELO NFL',
       version='0.1',
       description='NFL point spread and point total predictions.',
       author='J. Scott Moreland',
       author_email='morelandjs@gmail.com',
-      py_modules = ['melo_team']
+      py_modules = ['melo_nfl']
       )
